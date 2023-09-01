3e484814883c456067acef04137eae58627e2376
Gimena Medrano <fmedranop1@miumg.edu.gt>
2023-09-01 00:05:29 -0600

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Shift을(를) 두 번 눌러 Search Everywhere 대화상자를 열고 `show whitespaces`를 입력한 다음,
-// Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // 캐럿을 하이라이트된 텍스트에 놓고 Alt+Intro을(를) 누르면
-        // IntelliJ IDEA의 수정 제안을 볼 수 있습니다.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//LECTOR DE ENTRADA
 
-        // Mayús+F10을(를) 누르거나 여백의 녹색 화살표 버튼을 눌러 코드를 실행하세요.
-        for (int i = 1; i <= 5; i++) {
 
-            // Mayús+F9을(를) 눌러 코드 디버깅을 시작하세요. 중단점을 하나 설정해 드렸습니다.
-            // 언제든 Ctrl+F8을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
-        }
+        System.out.println("¡Bienvenido!");
+        System.out.println("Toppings Disponibles:");
+        CLSPizza.mostrarToppings(CLSPizza.inicializarToppingsDisponibles());
+
+
+        CLSPizza pizza = CLSPizza.crearPizza(reader,CLSPizza.inicializarToppingsDisponibles());
+
+        System.out.println(pizza.calculateTotalPrice());
+
+        CLSPizza.prepararPizza();
+
+        System.out.print("Por favor ingresa el valor de su billete: ");
+        double valor = CLSPizza.obtenerEnteroInput(reader);
+
+        System.out.println("Tu cambio es: " + (valor - pizza.calculateTotalPrice()));
     }
 }
+
--- src/Topping.java
+++ src/Topping.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Topping {
+public class Topping {
+
+    private String name;
+    private double price;
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
